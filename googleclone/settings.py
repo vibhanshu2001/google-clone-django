65d251469eab554242ce6c0b3ae3d9a7394ae788
Vibhanshu Garg <v2001.garg@gmail.com>
2021-01-31 16:57:43 +0530

    Upfate

--- googleclone/settings.py
+++ googleclone/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q62eci6-06h*gyy(z&_-f15#&ez9w!i#ndk9&qy4r04366ss_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','google-clone-django.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
